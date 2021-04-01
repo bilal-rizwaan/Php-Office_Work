1bb12a3b287c22215a494be84dcf0a53f476dde9
bilalcapripio <bilal.capripio@gmail.com>
2021-04-01 19:23:29 +0500

    Factorial Php

---
+++ Factorial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial in Php</title>
+</head>
+<body>
+<h2>
+    <?php
+    function fact($a){
+        $f=0;
+        $f= $f * $a;    
+    }
+    fact(5);
+    ?>
+</h2>
+    
+</body>
+</html>
