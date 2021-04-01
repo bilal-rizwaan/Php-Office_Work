cbf6e2d3dde67efbaf405e2dcb63059a8a1bef49
bilalcapripio <bilal.capripio@gmail.com>
2021-04-01 18:23:16 +0500

    2D-Array

---
+++ 2D.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2D Multi-dimensional Array Php</title>
+</head>
+<body>
+<?php
+$a=array(
+    array('Sam',21,sam@gmail.com)
+)
+
+?>
+    
+</body>
+</html>
