aba5676f51e9329912f069d8c912de7c4a8d0183
bilalcapripio <bilal.capripio@gmail.com>
2021-04-01 18:00:40 +0500

    Associative Array

---
+++ associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Array in Php</title>
+</head>
+<body>
+    <?php
+    $record=(
+        "Sam" => "21",
+        "Jerry" => "22",
+        "John" => "20",
+        "Harry" => "23",
+        "Manual" => "24"
+        )
+    
+    ?>
+</body>
+</html>
