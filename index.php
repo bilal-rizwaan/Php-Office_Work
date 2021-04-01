977c4ddd9436f5c324711601f8f3e1ecb5193a09
bilalcapripio <bilal.capripio@gmail.com>
2021-04-01 12:34:45 +0500

    Php Calculator

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$a=1;
+$b=1;
+$c=1;
+echo "<ul>";
+while($a<=10){
+    echo '<li>'. $a . 'Php <br>';
+    $a++;
+    echo "<ul>";
+
+}
+while($b<=10){
+    echo '<li>'. $b . 'Php <br>';
+    $b++;
+}
+echo "<ul>";
+while($c<=10){
+    echo '<li>'. $c . 'Php <br>';
+    $c++;
+    echo "<ul>";
+
+}
+?>
