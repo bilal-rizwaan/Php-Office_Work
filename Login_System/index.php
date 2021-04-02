e616d51d81377fcad35abd63c76359e054c3820e
bilalcapripio <bilal.capripio@gmail.com>
2021-04-02 16:57:57 +0500

    LogIn Sysyem

--- Login_System/index.php
+++ Login_System/index.php
@@ -4,22 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pattern in Php</title>
+    <title>Login System</title>
 </head>
 <body>
-    <?php
+    <form method="POST">
+
+    <input type="text" name="user">
+    <input type="submit" name="submit" value="Login">
+
+
+    </form>
     
-    for($i=1;$i<=10;$i++)
-    {
-        for($j=1;$j<=$i;$j++){
-            echo '*';
-            // echo $i;
-        }
-        // echo $j;
-        echo '</br>';
-    }
-    ?>
-
-
 </body>
 </html>
