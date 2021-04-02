6e5614bbc50ce0f99244ad6a3a80952ae6ff6757
bilalcapripio <bilal.capripio@gmail.com>
2021-04-02 12:45:27 +0500

    Fun Lv

---
+++ Fun.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fun Php</title>
+</head>
+<body>
+
+<form method="POST">
+    <input type="text" name="lv1">
+    <input type="text" name="lv2">
+    <input type="submit" name="submit" value="submit">
+    <pre>
+    <?php
+    if(isset($_POST['submit'])){
+
+    }
+    
+    ?>
+    </pre>
+</form>
+    
+</body>
+</html>
